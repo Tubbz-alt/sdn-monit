0586c7b8f2a033b47c156c67cf462810c94de9e8
roookieengineer <35984842+roookieengineer@users.noreply.github.com>
2019-04-06 07:49:37 +0700

    Update golang_setup.sh

--- server_side/golang_setup.sh
+++ server_side/golang_setup.sh
@@ -4,6 +4,13 @@ wget https://storage.googleapis.com/golang/go1.12.linux-armv6l.tar.gz
 sudo tar -xvf go1.12.linux-armv61.tar.gz
 sudo mv go /usr/local
 sudo mkdir go
+cd go
+sudo mkdir pkg
+sudo mkdir bin
+sudo mkdir src
+cd src
+sudo mkdir github.com
+cd $HOMW
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
