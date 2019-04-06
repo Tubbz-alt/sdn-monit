d29a3c3f9184b6e5c689cb75d32e17638ea351e5
roookieengineer <35984842+roookieengineer@users.noreply.github.com>
2019-04-06 10:52:12 +0700

    Update depedencies_setup.sh

--- server_side/depedencies_setup.sh
+++ server_side/depedencies_setup.sh
@@ -1,6 +1,14 @@
 #!/bin/sh -e
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install git git-flow python-dev python-setuptools python-pip python-pyt$
+sudo apt-get install git -y
+sudo apt-get install git-flow -y
+sudo apt-get install pytho-dev -y
+sudo apt-get install python-setuptools -y
+sudo apt-get install python-pip -y
+sudo apt-get install python-pytest -y
+sudo apt-get install python-yaml -y
+sudo apt-get install adduser -y
+sudo apt-get install libfontconfig -y
 cd $HOME
 sudo mkdir sdn-monit
