907e73c54600021cc96111c5638c2014d1a73ecf
roookieengineer <35984842+roookieengineer@users.noreply.github.com>
2019-04-08 10:44:13 +0700

    Update node-exporter_setup.sh

--- server_side/node-exporter_setup.sh
+++ server_side/node-exporter_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
-cd $GOPATH/src/github.com/prometheus
-git clone https://github.com/prometheus/node_exporter.git
-cd node_exporter
-make build
-cp node_exporter $HOME/sdn-monitA
+cd $HOME
+wget https://github.com/prometheus/node_exporter/releases/download/v0.17.0/node_exporter-0.17.0.linux-armv6.tar.gz
+tar xvzf node_exporter-0.17.0.linux-armv6.tar.gz
+cd node_exporter-0.17.0.linux-armv6
+chmod +x node_exporter
+cp node_exporter $HOME/sdn-monit
