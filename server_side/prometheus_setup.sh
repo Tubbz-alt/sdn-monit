ace583540b04729379661df89b8fed84a3dd3311
Rookie Engineer <35984842+roookieengineer@users.noreply.github.com>
2019-04-13 19:08:11 +0700

    Update prometheus_setup.sh

--- server_side/prometheus_setup.sh
+++ server_side/prometheus_setup.sh
@@ -7,15 +7,3 @@ chmod +x prometheus
 chmod +x promtool
 cp prometheus $HOME/sdn-monit
 cp promtool $HOME/sdn-monit
-sudo echo "global:" >> startx.yml
-sudo echo " scrape_interval:     15s" >> startx.yml
-sudo echo "" >> startx.yml
-sudo echo "scrape_configs:" >> startx.yml 
-sudo echo "  - job_name: 'se'" >> startx.yml >> startx.yml
-sudo echo "    static_configs:" >> startx.yml
-sudo echo "      - targets:" >> startx.yml
-sudo echo "        - 'localhost:9100'" >> startx.yml
-sudo echo "        - '10.11.12.2:9100'" >> startx.yml
-sudo echo "        - '10.11.12.3:9100'" >> startx.yml
-sudo echo "        - '10.11.12.4:9100'" >> startx.yml
-cp startx.yml $HOME/sdn-monit
