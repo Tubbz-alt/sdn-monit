7adf41fc2d0504874161234f56ffc9ddbbca2718
A . S . P . I <35984842+roookieengineer@users.noreply.github.com>
2019-07-03 11:11:03 +0700

    Update ovs_setup.sh

--- client_side/ovs_setup.sh
+++ client_side/ovs_setup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh -e
 cd $HOME
-wget http://openvswitch.org/releases/openvswitch-2.11..tar.gz
-tar -xvzf openvswitch-2.9.2.tar.gz
+wget http://openvswitch.org/releases/openvswitch-2.11.tar.gz
+tar -xvzf openvswitch-2.11.2.tar.gz
+
+cd openvswitch-2.11.2.tar.gz
+sudo ./configure
+sudo make -j4
+sudo make -j4 install
